336b886930c2bfc749b4a42f6d8ea4557af7771a
Abdurahmon <abdurahmonmirmaxsudov2804@gmail.com>
2023-08-01 12:03:33 +0500

    50/70

--- index.js
+++ index.js
@@ -19,6 +19,26 @@ function addNewStudent() {
     const birth = document.getElementById('dab').value;
     const ielts = document.getElementById('ielts').value;
 
+    if (ielts < 0 || ielts > 9) {
+        document.querySelector('h1').innerHTML = "Please enter a valid IELTS score";
+        document.getElementById('ielts').value = '';
+        return;
+    } else if (firstName == null || surname == null) {
+        document.querySelector('h1').innerHTML = "Please enter a correctly";
+        document.getElementById('firstName').value = '';
+        document.getElementById('surname').value = '';
+        return;
+    } else {
+        const bd = new Date(birth);
+        const ld = new Date();
+
+        if (bd > ld) {
+            document.querySelector('h1').innerHTML = "Please enter a correct birth date";
+            document.getElementById('dab').value = '';
+            return;
+        }
+    }
+
     const newStudent = {
         firstName: firstName,
         surname: surname,
@@ -28,17 +48,18 @@ function addNewStudent() {
 
     arrOfStudents.push(newStudent);
 
-    console.log(arrOfStudents);
-
-    document.getElementById('firstName').value = '';
-    document.getElementById('surname').value = '';
-    document.getElementById('dab').value = '';
-    document.getElementById('ielts').value = '';
-
+    changeAllToEmpty();
     updateDisplay();
 }
 
 function updateDisplay() {
     const displayElement = document.querySelector('h1');
     displayElement.textContent = "List of students: " + JSON.stringify(arrOfStudents);
+}
+
+function changeAllToEmpty() {
+    document.getElementById('firstName').value = '';
+    document.getElementById('surname').value = '';
+    document.getElementById('dab').value = '';
+    document.getElementById('ielts').value = '';
 }
